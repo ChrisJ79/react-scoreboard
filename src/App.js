a0182380e30bdca676c3c3abae5a87115c08f7ee
Chris <csjohnson79@icloud.com>
2020-01-18 15:35:23 -0700

    Built out App, Player, and Counter components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class Header extends Component {
-  render() {
+const App = () => {
+	return(
+		<div className="scoreboard">
+			<Header />
+			<Player />
+		</div>
+	);
+}
+
+
+const Header = () => {
     return (
       <header>
         <h1>Scoreboard</h1>
         <span className="stats">Players: 1</span>
       </header>
     );
-  }
 }
 
-export default Header;
+const Player = () => {
+	return(
+		<div className="player">
+			<span className="player-name">
+				Chris
+			</span>
+			<Counter />
+		</div>
+	);
+}
+
+const Counter = () => {
+	return(
+		<div className="player">
+			<div className="counter">
+				<button className="counter-action decrement"> - </button>
+				<span className="counter-score">0</span>
+				<button className="counter-action increment"> + </button>
+			</div>
+		</div>
+	);
+}
+
+
+export default App;
