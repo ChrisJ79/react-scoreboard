d4fb479b3787463e22c6875e2769e258e2cb1a5b
Chris <csjohnson79@icloud.com>
2020-01-21 19:11:18 -0700

    Updates

---
+++ precache-manifest.c08cc2078a996914d3b331b572e020e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aa6b82c05263d0498fdbb6fddd9c9f6",
+    "url": "/react-scoreboard/index.html"
+  },
+  {
+    "revision": "54b1674381e08227737b",
+    "url": "/react-scoreboard/static/css/main.6c9fc71a.chunk.css"
+  },
+  {
+    "revision": "05aa5c4c44f4e598bdd2",
+    "url": "/react-scoreboard/static/js/2.696184dd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-scoreboard/static/js/2.696184dd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "54b1674381e08227737b",
+    "url": "/react-scoreboard/static/js/main.1299e81a.chunk.js"
+  },
+  {
+    "revision": "fa38441a174ff182b53e",
+    "url": "/react-scoreboard/static/js/runtime-main.a3718bd7.js"
+  }
+]);
